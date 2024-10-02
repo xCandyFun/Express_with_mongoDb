5bddac5cc74a3fce1d08e81e153bd182c2eca0d7
Simon T. <simtho0624@gmail.com>
2024-10-02 08:43:18 +0200

    Add mongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/carworkshop')
+.then(()=> console.log('Connected to MongoDB'))
+.catch(err => console.error('Connect error', err));
+
+const Car = mongoose.model('Car', new mongoose.Schema({
+    registrationNumber: String,
+    make: String,
+    model: String,
+    year: Number
+}));
 
 const cars = [
     {registrationNumber: 'ABC123', make: 'Volvo', model: 'XC60', year: 2018},
@@ -12,15 +23,17 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Car Workshop API!');
 });
 
-app.get('/cars', (req,res) => {
+app.get('/cars', async (req,res) => {
+
+    const cars = await Car.find();
     res.json(cars);
 });
 
-app.get('/cars/:registrationNumber', (req,res) => {
-    const car = cars.find(c => c.registrationNumber === req.params.registrationNumber);
+app.get('/cars/:registrationNumber', async (req,res) => {
+    const car = await Car.findOne({ registrationNumber: req.params.registrationNumber });
 
     if (car) {
-    res.send(`Car found: ${car.make} ${car.model} from ${car.year}`)
+    res.json(car)
     }else {
         res.status(404).send('Car not found')
     }
